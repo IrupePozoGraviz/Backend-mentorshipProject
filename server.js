572b1a6a02a3769253b92b292dce57540bc5fa6d
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-06-01 17:33:06 +0200

    a bunch of code

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const io = new Server(server); // Create the Socket.IO server
 app.use(cors());
 app.use(express.json());
 
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/mentorship";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
